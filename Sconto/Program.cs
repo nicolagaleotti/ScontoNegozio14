6fc59129e3ec3d6d1bf64ce6d263150b01b33fa5
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 13:03:20 +0200

    fine programma

--- Sconto/Program.cs
+++ Sconto/Program.cs
@@ -6,7 +6,28 @@ namespace Sconto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Inserisci il prezzo del primo prodotto: ");
+            double p1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Inserisci il prezzo del secondo prodotto: ");
+            double p2 = double.Parse(Console.ReadLine());
+            double sconto;
+            double scontato;
+            double totale;
+            if (p1 > p2)
+            {
+                sconto = p2 / 100 * 50;
+                scontato = p2 - sconto;
+                totale = p1 + scontato;
+                Console.WriteLine($"Lo sconto è di {sconto} euro e il prezzo da pagare {totale} euro");
+            }
+            else
+            {
+                sconto = p1 / 100 * 50;
+                scontato = p1 - sconto;
+                totale = p2 + scontato;
+                Console.WriteLine($"Lo sconto è di {sconto} euro e il prezzo da pagare {totale} euro");
+            }
+            Console.ReadLine();
         }
     }
 }
